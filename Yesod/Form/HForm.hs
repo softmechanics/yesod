3c9456a64451fa3afcc43f64e3da202911e09abd
Matt Brown <matt@softmechanics.net>
2010-11-04 23:55:30 -0700

    added type signatures and type families for clarity

--- Yesod/Form/HForm.hs
+++ Yesod/Form/HForm.hs
@@ -5,17 +5,17 @@
            , UndecidableInstances
            , FunctionalDependencies
            , OverlappingInstances
+           , ScopedTypeVariables
+           , GADTs
            #-}
 module Yesod.Form.HForm where
 
 import Yesod.Form
+import Yesod.Handler
 
 import Data.HList
-import Data.HList.Record 
 import Data.HList.Utils
 import Data.HList.Tuple hiding (Construct(..))
-import Data.HList.Field
-import Data.HList.Field.Label
 import Data.HList.Field.Constructor
 
 import Control.Applicative
@@ -30,9 +30,21 @@ type instance FormData' (HCons f fs) = (HCons (FormDataField f) (FormData' fs))
 type family FormDataField a
 type instance FormDataField (LVPair nm fld) = (LVPair nm (FieldData fld))
 
+type HFormHandler f = GHandler (HFormSub f) (HFormMaster f) 
+
+type family HFormSub f
+type instance HFormSub (Record (HCons (LVPair l field) fs)) = FieldSub field
+
+type family HFormMaster f
+type instance HFormMaster (Record (HCons (LVPair l field) fs)) = FieldMaster field
+
+type FieldHandler f = GHandler (FieldSub f) (FieldMaster f)
+type FieldFormField f = FormField (FieldSub f) (FieldMaster f)
+
 class (FieldTyImpl (FieldData fld)
       ) => GFormField fld where
-  type FieldForm fld
+  type FieldSub fld
+  type FieldMaster fld
   type FieldData fld 
   type FieldName fld
 
@@ -40,14 +52,54 @@ class (FieldTyImpl (FieldData fld)
 
   formFieldDefault :: fld -> Maybe (FieldData fld)
   formFieldDefault _ = Nothing
-  
-  formFieldImpl :: (Monad m) => fld -> m (FormField sub y (FieldData fld))
-  formFieldImpl fld = return $ fieldTyImpl sets def
+
+  formField :: fld -> FieldFormField fld (FieldData fld)
+  formField fld = fieldTyImpl sets def
       where def = formFieldDefault fld
             sets = formFieldSettings fld
 
+  formFieldParser :: fld -> FieldHandler fld (FieldFormField fld (FieldData fld))
+  formFieldParser = formFieldRenderer
+
+  formFieldRenderer :: fld -> FieldHandler fld (FieldFormField fld (FieldData fld))
+  formFieldRenderer = return . formField
+
   formFieldChecker :: fld -> Maybe (a -> Either String b)
   formFieldChecker _ = Nothing
+
+  setFieldValue :: fld -> (FieldData fld) -> fld
+
+instance (FieldTyImpl t) => GFormField (HField sub y nm t) where
+  type FieldSub (HField sub y nm t) = sub
+  type FieldMaster (HField sub y nm t) = y
+  type FieldData (HField sub y nm t) = t
+  type FieldName (HField sub y nm t) = nm
+ 
+  formFieldSettings (IntField _ s _) = s
+  formFieldSettings (StringField _ s _) = s
+
+  formFieldDefault (IntField _ _ d) = d
+  formFieldDefault (StringField _ _ d) = d
+
+  setFieldValue (IntField nm s _) v = IntField nm s $ Just v
+  setFieldValue (StringField nm s _) v = StringField nm s $ Just v
+
+instance (FieldTyImpl t
+         ,Eq t
+         ) => GFormField (SelectField sub y nm t) where
+  type FieldSub (SelectField sub y nm t) = sub
+  type FieldMaster (SelectField sub y nm t) = y
+  type FieldData (SelectField sub y nm t) = t
+  type FieldName (SelectField sub y nm t) = nm
+ 
+  formFieldDefault (SelectField _ _ _ d) = d
+
+  formFieldSettings (SelectField _ settings _ _) = settings
+  formFieldRenderer (SelectField _ settings opts _) = do
+      opts' <- opts
+      return $ selectField opts' settings Nothing
+
+  setFieldValue (SelectField nm s o _) v = SelectField nm s o $ Just v
 
 class FieldTyImpl a where
   fieldTyImpl :: FormFieldSettings -> FormletField sub y a
@@ -58,84 +110,177 @@ instance FieldTyImpl Int where
 instance FieldTyImpl [Char] where
   fieldTyImpl = stringField
 
-data FormFieldImpl = FormFieldImpl
+data HFormFieldParser = HFormFieldParser
+data HFormFieldRenderer = HFormFieldRenderer
+
 
 instance (GFormField f
-         ,Monad m
+         ,h ~ FieldHandler f
+         ,Monad h
          ,d ~ FieldData f
-         ,impl ~ FormField sub y d
-         ) => Apply FormFieldImpl f (m impl) where
-  apply _ f = formFieldImpl f
+         ,impl ~ (FieldFormField f) d
+         ) => Apply HFormFieldParser f (h impl) where
+  apply _ f = formFieldParser f
 
-data IntField nm = IntField nm FormFieldSettings 
-data SelectField nm h t = SelectField nm FormFieldSettings (h [(t,String)]) 
+instance (GFormField f
+         ,h ~ FieldHandler f
+         ,Monad h
+         ,d ~ FieldData f
+         ,impl ~ (FieldFormField f) d
+         ) => Apply HFormFieldRenderer f (h impl) where
+  apply _ f = formFieldRenderer f
 
-instance (
-         ) => GFormField (QualField form (IntField nm)) where
-  type FieldForm (QualField form (IntField nm)) = form
-  type FieldData (QualField form (IntField nm)) = Int
-  type FieldName (QualField form (IntField nm)) = nm
- 
-  formFieldSettings (QualField _ (IntField _ settings)) = settings
+data FormFieldF = FormFieldF
+instance (GFormField f
+         ,d ~ FieldData f
+         ,fld ~ FieldFormField f d
+         ) => Apply FormFieldF f fld where
+  apply _ f = formField f
 
-instance (FieldTyImpl t
-         ,Eq t
-         ) => GFormField (QualField form (SelectField nm h t)) where
-  type FieldForm (QualField form (SelectField nm h t)) = form
-  type FieldData (QualField form (SelectField nm h t)) = t
-  type FieldName (QualField form (SelectField nm h t)) = nm
- 
-  formFieldSettings (QualField _ (SelectField _ settings _)) = settings
+data HField sub y nm t where
+    IntField :: nm -> FormFieldSettings -> Maybe Int -> HField sub y nm Int
+    StringField :: nm -> FormFieldSettings -> Maybe String -> HField sub y nm String
 
-  formFieldImpl (QualField _ (SelectField _ settings opts)) = do
-                   opts' <- opts
-                   return $ selectField opts' settings Nothing
+data SelectField sub y nm t = SelectField nm FormFieldSettings (GHandler sub y [(t,String)]) (Maybe t)
 
-data QualFieldF a = QualFieldF a
-data QualField a b = QualField a b
+data FieldToLVPair = FieldToLVPair
 
-instance (MkQualField form field
-         ,QualField form field ~ field'
-         ,nm ~ FieldName field'
-         ) => Apply (QualFieldF form) field (LVPair nm field') where
-  apply (QualFieldF form) field = LVPair $ mkQualField form field
+instance (nm ~ FieldName f) => Apply FieldToLVPair f (LVPair nm f) where
+  apply _ f = LVPair f
 
-class MkQualField form field where
-  mkQualField :: form -> field -> QualField form field
-  
-instance (
-         ) => MkQualField form (SelectField nm h t) where
-  mkQualField form field = QualField form field
+data InitFormField = InitFormField
 
-instance MkQualField form field where
-  mkQualField form field = QualField form field
-
-class MkQualForm form fields qual | form fields -> qual where
-  qualForm :: form -> fields -> qual
-
-instance (Untuple fields fields'
-         ,HMap (QualFieldF form) fields' qualFlds
-         ,Monad (FormHandler form)
-         ) => MkQualForm form fields (Record qualFlds) where
-  qualForm form fields = Record $ hMap (QualFieldF form) (untuple fields)
+hForm :: (Untuple t l
+         ,HMap FieldToLVPair l l'
+         ) => t -> Record l'
+hForm = Record . hMap FieldToLVPair . untuple
 
 formData :: f -> FormData f
 formData _ = undefined
 
-formConstructor :: (Construct (FormData f)
-                   ,Apply Constructor (FormData f) c
+formConstructor :: (Apply Constructor (FormData f) c
                    ) => f -> c
 formConstructor = constructor . formData
 
-formFieldlets :: (RecordValues r vs
-                 ,HMap FormFieldImpl vs vs'
-                 ,HSequence vs' vs''
-                 ) => Record r -> vs''
-formFieldlets = hSequence . hMap FormFieldImpl . recordValues
+hFormFields :: (RecordValues r vs
+               ,HMap f vs vs'
+               ,HSequence vs' vs''
+               ) => f -> Record r -> vs''
+hFormFields f = hSequence . hMap f . recordValues
 
-hFormlet f = do fieldlets <- formFieldlets f
-                return $ hFoldl ApplicativeF (pure $ formConstructor f) fieldlets
+formFields :: (RecordValues r vs
+              ,HMap FormFieldF vs vs'
+              ,HSequence vs' vs''              
+              ) => Record r -> vs''
+formFields = hFormFields FormFieldF
 
-runHFormGet f = do fl <- hFormlet f
-                   runFormGet fl
+class HFormParsers f ps | f -> ps where
+  hFormParsers :: f -> ps
 
+instance (RecordValues r vs
+         ,HMap HFormFieldParser vs vs'
+         ,HSequence vs' vs''
+         ) => HFormParsers (Record r) vs'' where
+  hFormParsers = hFormFields HFormFieldParser
+
+class HFormRenderers f rs | f -> rs where
+  hFormRenderers :: f -> rs
+
+instance (RecordValues r vs
+         ,HMap HFormFieldRenderer vs vs'
+         ,HSequence vs' vs''
+         ) => HFormRenderers (Record r) vs'' where
+  hFormRenderers = hFormFields HFormFieldRenderer
+
+type HGForm f xml = GForm (HFormSub f) (HFormMaster f) xml (FormData f)
+
+-- | ConstructHForm
+class ConstructHForm form fields xml | form fields -> xml where
+  constructHForm :: form -> fields -> (HGForm form xml)
+
+instance (Apply Constructor (FormData form) ctor
+         ,Applicative f
+         ,HFoldl ApplicativeF (f ctor) fields (HGForm form xml)
+         ) => ConstructHForm form fields xml where
+  constructHForm = runHFormConstructor . formConstructor
+    where runHFormConstructor ctor fields = hFoldl ApplicativeF (pure ctor :: f ctor) fields
+
+-- | ParseHFormlet
+class ParseHFormlet f xml | f -> xml where
+  parseHFormlet :: f -> (HFormHandler f) (HGForm f xml)
+
+instance (Monad m
+         ,HFormParsers f (m ps)
+         ,ConstructHForm f ps xml
+         ,m ~ HFormHandler f
+         ) => ParseHFormlet f xml where
+  parseHFormlet f = do ps <- hFormParsers f
+                       return $ constructHForm f ps
+
+class RenderHFormlet f xml | f -> xml where
+  renderHFormlet :: f -> (HFormHandler f) (HGForm f xml)
+
+instance (m ~ HFormHandler f
+         ,Monad m
+         ,HFormRenderers f (m rs)
+         ,ConstructHForm f rs xml
+         ) => RenderHFormlet f xml where
+  renderHFormlet f = do fs <- hFormRenderers f
+                        return $ constructHForm f fs
+
+class ParseHFormGet f xml | f -> xml where
+  parseHFormGet :: f -> (HFormHandler f) (FormResult (FormData f), xml, Enctype)
+
+instance (ParseHFormlet f xml
+         ) => ParseHFormGet f xml where
+  parseHFormGet f = do fl <- parseHFormlet f
+                       runFormGet fl
+
+renderHFormGet :: (RenderHFormlet f xml
+                  ,sub ~ HFormSub f
+                  ,y ~ HFormMaster f
+                  ,h ~ HFormHandler f
+                  ) => f -> (HGForm f xml -> HGForm f xml') -> h (FormResult (FormData f), xml', Enctype)
+renderHFormGet f r = do fl <- renderHFormlet f
+                        runFormGet $ r fl
+
+class HUpdateForm form values form' | form values -> form' where
+  hUpdateForm :: form -> values -> form'
+
+instance HUpdateForm form HNil form where
+  hUpdateForm f _ = f
+
+instance (HUpdateForm form values form'
+         ) => HUpdateForm (Record form) (Record values) (Record form') where
+  hUpdateForm (Record f) (Record vs) = Record $ hUpdateForm f vs
+
+instance (ApplyAtLabel (SetFieldValue v) l form form'
+         ,HUpdateForm form' values form''
+         ) => HUpdateForm form (HCons (LVPair l v) values) form'' where
+  hUpdateForm form (HCons (LVPair v) values) = form''
+      where form' = applyAtLabel (SetFieldValue v) (undefined::l) form
+            form'' = hUpdateForm form' values
+
+data SetFieldValue v = SetFieldValue v
+instance (GFormField fld
+         ,fld ~ fld'
+         ,v ~ FieldData fld
+         ) => Apply (SetFieldValue v) fld fld' where
+  apply (SetFieldValue v) fld = setFieldValue fld v
+
+hUpdateForm' :: (HZip f d a
+               ,HMap SetFieldValues a f'
+               ,f ~ f'
+               ,d ~ FormData' f
+               ) => Record f -> Record d -> Record f
+hUpdateForm' (Record f) (Record d) = Record . hMap SetFieldValues $ hZip f d
+
+data SetFieldValues = SetFieldValues
+
+instance (GFormField fld
+         ,n ~ n'
+         ,fld ~ fld'
+         ,v ~ FieldData fld
+         ) => Apply SetFieldValues ((LVPair n fld),(LVPair n' v)) (LVPair n fld') where
+    apply _ (LVPair fld,LVPair v) = LVPair $ setFieldValue fld v
+    
